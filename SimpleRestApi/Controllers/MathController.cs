247e01591c60b2df9ba295a9fc786a4b6c089eb1
bestdavid5509 <dtbest5@gmail.com>
2025-07-02 16:14:34 -0400

    Added simple controller to showcase Rest API

---
+++ SimpleRestApi/Controllers/MathController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleRestApi.Controllers {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MathController : ControllerBase {
+        [HttpGet("sum")]
+        public IActionResult GetSum(int num1, int num2) {
+            var result = num1 + num2;
+            return Ok(new { sum = result });
+        }
+    }
+}
